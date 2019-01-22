0b16a619d6349f66f4b6c7e52d3bb809810adbb6
Christian Nytra <ch.nytra.13@gmail.com>
2019-01-22 11:55:31 +0100

    Temperaturrechner hinzugefügt

---
+++ TemperaturRechner/TemperaturRechner/Converter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperaturRechner
+{
+    public class Converter
+    {
+        public string ConvertTemperature(string inputTemp)
+        {
+            var einheit = inputTemp.Substring(inputTemp.Length - 1).ToLower();
+            var eingangsTemperatur = Convert.ToInt32(inputTemp.Remove(inputTemp.Length - 1));
+
+            switch (einheit)
+            {
+                case "f":
+                    return $"Die eingegebene Temperatur entspricht {(eingangsTemperatur - 32) * 5 / 9}°C\n";
+                case "c":
+                    return $"Die eingegebene Temperatur entspricht {eingangsTemperatur * 9 / 5 + 32}°F\n";
+                default:
+                    throw new Exception("Es sind nur Celsius(C) und Fahrenheit(F) erlaubt!!!!!!!");
+            }
+        }
+    }
+}
---
+++ TemperaturRechner/TemperaturRechner/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperaturRechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var temperaturRechner = new Converter();
+
+            Console.WriteLine("Geben Sie eine Temperatur mit passendem Einheitenkürzel(Celsius und Fharenheit erlaubt) an.\n \nBeispiel:\t28C \n\t\tfür 28 grad Celsius");
+
+            Console.WriteLine(temperaturRechner.ConvertTemperature(Console.ReadLine()));
+        }
+    }
+}
