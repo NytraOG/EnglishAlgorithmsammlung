8ad64df21cfa86c0cb1e16b8e31acde325f90af0
Christian Nytra <ch.nytra.13@gmail.com>
2019-01-22 11:28:42 +0100

    Teilbarkeitschecker added

---
+++ GlattTeilbareZahlen/GlattTeilbareZahlen/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlattTeilbareZahlen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var teilbarChecker = new TeilbarChecker();
+
+            Console.WriteLine("Überprüfung der glatten Teilbarkeit zweier Zahlen in eine ganze Zahl. \n\nQuotient(integer) eingeben:");
+            var quotient = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Divisor(integer) eingeben:");
+            int divisor = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine(teilbarChecker.CheckTeilbarkeit(quotient, divisor)
+                ? $"\n{quotient}/{divisor} ist glatt teilbar. \nErgebnis: {quotient / divisor}\n"
+                : $"\n{quotient}/{divisor} ist nicht ohne Rest teilbar.\n");
+        }
+    }
+}
---
+++ GlattTeilbareZahlen/GlattTeilbareZahlen/TeilbarChecker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlattTeilbareZahlen
+{
+    public class TeilbarChecker
+    {
+        public bool CheckTeilbarkeit(int quotient, int divisor)
+        {
+            return quotient%divisor == 0;
+        }
+    }
+}
