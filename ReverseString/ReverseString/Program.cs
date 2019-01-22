f9d16791b5d80ef4eeb06db4d612dc5a6d004e76
Christian Nytra <ch.nytra.13@gmail.com>
2019-01-22 12:30:19 +0100

    Stringtwister added

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var twister = new StringTwister();
+
+            Console.WriteLine("Beliebigen Text eingeben:\n");
+            Console.WriteLine(twister.TwistString(Console.ReadLine()));
+        }
+    }
+}
---
+++ ReverseString/ReverseString/StringTwister.cs
@@ -0,0 +1,17 @@
+﻿namespace ReverseString
+{
+    public class StringTwister
+    {
+        public string TwistString(string inputString)
+        {
+            string outputString = "";
+
+            for (int i = inputString.Length - 1; i >= 0; i--)
+            {
+                outputString += inputString.Substring(i, 1);
+            }
+
+            return outputString;
+        }
+    }
+}
