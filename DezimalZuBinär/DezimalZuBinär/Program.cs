421584fadf7997e5f293d4c427917b5a9411392a
Christian Nytra <ch.nytra.13@gmail.com>
2019-04-10 10:15:54 +0200

    .

--- DezimalZuBinär/DezimalZuBinär/Program.cs
+++ DezimalZuBinär/DezimalZuBinär/Program.cs
@@ -11,14 +11,19 @@ namespace DecToBin
     {
         static void Main(string[] args)
         {
-            var converter = new Converter();
-            var outputter = new OutputGenerator();
-
             Console.WriteLine("Zum konvertieren eine ganze Zahl im Bereich [0 - 255] eingeben und bestätigen.");
-            var binärArray = converter.ConvertInteger(Convert.ToInt32(Console.ReadLine()));
-            var binärOutput = "Deine eingegebene Zahl entspricht dieser binären Form : " + outputter.GenerateString(binärArray);
 
-            Console.WriteLine(binärOutput);
+            do
+            {
+                var converter = new Converter();
+                var outputter = new OutputGenerator();
+
+                var binärArray  = converter.ConvertInteger(Convert.ToInt32(Console.ReadLine()));
+                var binärOutput = "Deine eingegebene Zahl entspricht dieser binären Form : " + outputter.GenerateString(binärArray);
+
+                Console.WriteLine(binärOutput);
+
+            } while (true);
         }
     }
 }
